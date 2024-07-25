94fbb4a9f27c7fc4f500b9785bf2ab7f7dd906b2
Thaisombra <thaisbatista311@gmail.com>
2024-07-25 18:36:26 -0300

    feat: adição da classe membro e estrutura do projeto

---
+++ src/main/java/models/entity/Membro.java
@@ -0,0 +1,40 @@
+package models.entity;
+
+public class Membro {
+    private String nome;
+    private String cpf;
+    private String senha;
+    private String matricula;
+    
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getMatricula() {
+        return matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        this.matricula = matricula;
+    }
+}
